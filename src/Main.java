acf13b90ff290c1e79c52206f0fe392883c3d535
IgorBerger03 <igor.berger98@yandex.ru>
2022-07-19 16:26:11 +0500

    HW 4 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        int n = 11;
+        while (n > 1) {
+            n--;
+            System.out.print(n + " ");
+        }
+
+        // Задание 2
+        System.out.print("\n");
+        int friday = 1;
+        while (friday < 32) {
+            System.out.println("Сегодня пятница, " + friday + " -е число. Необходимо подготовить отчет.");
+            friday = friday + 7;
+        }
+
+        // Задание 3
+        int year = 2022;
+        int startYear = year - 200;
+        int endYear = year + 100;
+        for (year = startYear; year <= endYear; year++) {
+            if  (year % 79 == 0) {
+                System.out.println(year);
+            }
+
+        }
+
+
+
 
     }
 }
